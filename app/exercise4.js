a593d91a4ff2d9b20482aed8706c5f897c035697
Dangerbooy <nuka91@interia.pl>
2016-04-08 09:53:30 +0200

    I finished task fourth

--- app/exercise4.js
+++ app/exercise4.js
@@ -1,8 +1,43 @@
 (function ()
 {
     'use strict';
+    window.exercise4 = {
+        countVowels: function(text) {
+            var vowels = ['a', 'e', 'y', 'u', 'i', 'o', 'ó', 'ą', 'ę'];
+            var counter = 0;
+            var text_male = text.toLowerCase();
+            if (typeof text_male === 'string') {
+                for (var i = 0; i < text_male.length; i++) {
+                    for (var x = 0; x < vowels.length; x++) {
+                        if (text_male[i] == vowels[x]) {
+                            counter++;
+                        }
+                    }
+                }
+                return counter;
+            } else return false;
 
-    // You can extend prototypes here.
+        },
+        arrayToString: function(array)
+        {
+            var inscription='';
+            for(var i=0;i<array.length;i++) {
+                if(i== 0)inscription=inscription+array[i].toString();
+                else inscription=inscription+' '+array[i].toString();
+            }
+            return inscription;
+        },
+
+    };
+    Number.prototype.add = function(number)
+    {
+        return this + number;
+    }
+    Number.prototype.isInTheRange = function (a,b) {
+        if(this >=a && this<=b)return true;
+        if(a>b) return undefined;
+        if(this <a || this > b) return false;
+    }
 
 
 })();
--- test/unit/exercise4.spec.js
+++ test/unit/exercise4.spec.js
@@ -8,13 +8,13 @@
         {
             it('should return count of vowels ', function ()
             {
-                expect('I am some text'.countVowels()).toEqual(5);
-                expect('aeiouy'.countVowels()).toEqual(6);
-                expect('AEIOUYX'.countVowels()).toEqual(6);
+                expect(exercise4.countVowels('I am some text')).toEqual(5);
+                expect(exercise4.countVowels('aeiouy')).toEqual(6);
+                expect(exercise4.countVowels('AEIOUYX')).toEqual(6);
             });
             it('should return 0 for empty string', function ()
             {
-                expect(''.countVowels()).toEqual(0);
+                expect(exercise4.countVowels('')).toEqual(0);
             });
         });
 
@@ -22,24 +22,24 @@
         {
             it('should return string from the array of strings', function ()
             {
-                expect(['one', 'two', 'three'].arrayToString()).toEqual('one two three');
+                expect(exercise4.arrayToString(['one', 'two', 'three'])).toEqual('one two three');
             });
             it('should return string from the array of strings and numbers', function ()
             {
-                expect(['one', 2, 'three', 4].arrayToString()).toEqual('one 2 three 4');
+                expect(exercise4.arrayToString(['one', 2, 'three', 4])).toEqual('one 2 three 4');
             });
             it('should return from the array of different types of elements', function ()
             {
-                expect([false, 0, 'one', 2].arrayToString()).toEqual('false 0 one 2');
+                expect(exercise4.arrayToString([false, 0, 'one', 2])).toEqual('false 0 one 2');
             });
         });
 
         describe('add', function ()
         {
             var numbers = [1, -2, 10, 4, 5];
-
             it('should add number 10 to number 1', function ()
             {
+
                 expect(numbers[0].add(10)).toEqual(11);
             });
             it('should add number 15 to number 10', function ()
